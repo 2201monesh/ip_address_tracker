249e807054d5df78977385838e254a65438df238
Monesh Goyal <goyalmonesh2201@gmail.com>
2024-02-15 04:19:26 +0530

    Added leaflet and map container

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client"
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useState } from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import 'leaflet/dist/leaflet.css'
 
 export default function Home() {
 
@@ -10,8 +11,11 @@ export default function Home() {
 
   // using Abstract GeolocationAPI - 
 
-  const apiURL = 'https://ipgeolocation.abstractapi.com/v1/'
-  const apiKey = 'b753b0662a634ff8862c531dbfd14303';
+  // const apiURL = 'https://ipgeolocation.abstractapi.com/v1/'
+  // const apiKey = 'b753b0662a634ff8862c531dbfd14303';
+
+  const apiURL = process.env.NEXT_PUBLIC_API_URL;
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
   const getUserLocationFromAPI = async () => {
   try {
@@ -20,21 +24,28 @@ export default function Home() {
     if (!response.ok) {
       throw new Error('Something went wrong getting Geolocation from API!');
     }
-
     const data = await response.json();
     console.log(data);
+    setLat(data.latitude);
+    setLong(data.longitude);
   } catch (error) {
     setError(error.message);
   }
 };
 
-  useEffect(() => {
-    // getUserLocationFromAPI();
-  }, [])
-
   return (
     <div>
+      <input type="text" onChange={(e) => setIp(e.target.value)} />
+      <button onClick={getUserLocationFromAPI}>click</button>
       <p>Your coordinates are: {lat} and {long}</p>
+
+      {lat && <MapContainer center={[lat, long]} zoom={13} >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+      <Marker position={[lat, long]} />
+      </MapContainer>}
     </div>
   );
 }
