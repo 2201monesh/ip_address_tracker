e4dc16f86626739334cd1185b428c372f349467e
Monesh Goyal <goyalmonesh2201@gmail.com>
2024-02-15 01:38:38 +0530

    getting user's location locally

--- app/page.js
+++ app/page.js
@@ -1,8 +1,34 @@
+"use client"
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [lat, setLat] = useState(null);
+  const [long, setLong] = useState(null);
+
+  const geolocationAPI = navigator.geolocation;
+
+  const getUserCoordinates = () => {
+    if (!geolocationAPI) {
+      setError('Geolocation API is not available in your browser!')
+    } else {
+      geolocationAPI.getCurrentPosition((position) => {
+        const { coords } = position;
+        setLat(coords.latitude);
+        setLong(coords.longitude);
+      }, (error) => {
+        setError('Something went wrong getting your position!')
+      })
+    }
+  }
+
+  useEffect(() => {
+    getUserCoordinates();
+  }, [])
+
   return (
     <div>
-      hello
+      <p>Your coordinates are: {lat} and {long}</p>
     </div>
   );
 }
