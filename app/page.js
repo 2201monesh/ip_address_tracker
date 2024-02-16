db050b879f59cc211d28923fa2a83a71d16ebb91
Monesh Goyal <goyalmonesh2201@gmail.com>
2024-02-16 16:12:28 +0530

    Configured map and styling

--- app/page.js
+++ app/page.js
@@ -3,12 +3,17 @@ import { useState } from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import 'leaflet/dist/leaflet.css'
 import { Icon } from "leaflet";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 export default function Home() {
 
   const [lat, setLat] = useState(null);
   const [long, setLong] = useState(null);
   const [ip, setIp] = useState(null);
+  const [ipAddress, setIpAddress] = useState(null);
+  const [city, setCity] = useState(null);
+  const [timezone, setTimezone] = useState(null);
+  const [isp, setIsp] = useState(null);
 
   // using Abstract GeolocationAPI - 
 
@@ -26,8 +31,12 @@ export default function Home() {
     console.log(data);
     setLat(data.latitude);
     setLong(data.longitude);
+    setIpAddress(data.ip_address);
+    setCity(data.city);
+    setTimezone(data.timezone.name);
+    setIsp(data.connection.isp_name);
   } catch (error) {
-    setError(error.message);
+    console.log(error.message);
   }
 };
 
@@ -37,18 +46,44 @@ export default function Home() {
   })
 
   return (
-    <div>
-      <input type="text" onChange={(e) => setIp(e.target.value)} />
-      <button onClick={getUserLocationFromAPI}>click</button>
-      <p>Your coordinates are: {lat} and {long}</p>
-
-      {lat && <MapContainer center={[lat, long]} zoom={13} >
+    <div className="h-screen w-screen flex flex-col">
+      <div className="w-screen border flex flex-col items-center pt-8 bg-cover bg-center bg-gradient-to-r from-purple-600 to-blue-600">
+        <p className="text-2xl mb-3">IP Address Tracker</p>
+        <div className="flex justify-center items-center mb-5">
+          <input className="w-80 p-3 outline-none rounded-s-md bg-white text-gray-500" type="text" placeholder="Search for any IP address" onChange={(e) => setIp(e.target.value)} />
+          <div className="bg-black text-white rounded-e-md p-2 cursor-pointer" onClick={getUserLocationFromAPI}><MdKeyboardArrowRight size={32} /></div>
+        </div>
+        <div className="w-4/5 h-32 px-28 py-2 border bg-white rounded-md flex justify-between items-center">
+          <div>
+            <p className="mb-2 font-semibold">IP ADDRESS</p>
+            <p>{ipAddress}</p>
+          </div>
+          <div>
+            <p className="mb-2 font-semibold">LOCATION</p>
+            <p>{city}</p>
+          </div>
+          <div>
+            <p className="mb-2 font-semibold">TIMEZONE</p>
+            <p>{timezone}</p>
+          </div>
+          <div>
+            <p className="mb-2 font-semibold">ISP</p>
+            <p>{isp}</p>
+          </div>
+        </div>
+      </div>
+      
+      <div className="w-screen">
+        {lat && <MapContainer center={[lat, long]} zoom={13} >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-      <Marker position={[lat, long]}icon={customIcon} />
-      </MapContainer>}
+         <Marker position={[lat, long]}icon={customIcon} />
+        </MapContainer>}
+        {!lat && <p className="flex justify-center">Map will we shown here</p>}
+      </div>
+
     </div>
   );
 }
